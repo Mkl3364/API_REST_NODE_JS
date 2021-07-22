47d7c750eac5556fc878f3d7e0a706a250b684e0
Mickael Brunet <mickael.brunet64@gmail.com>
2021-07-22 22:35:53 +0200

    Ajout de la méthode PUT afin de modifier leses valeurs du fichier JSON

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 var mysql = require('mysql');
 const parkings = require('./bdd.json')
 
+// Middleware : rend possible l'accès au body des requêtes POST, PUT, PATCH
+app.use(express.json())
 
 app.get('/parkings', (req,res) => {
     res.status(200).json(parkings)
@@ -14,7 +16,19 @@ app.get('/parkings/:id', (req,res) => {
     res.status(200).json(parking)
 })
 
+app.post('/parkings', (req, res) => {
+    parkings.push(req.body)
+    res.status(200).json(parkings)
+})
 
+app.put('/parkings/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    let parking = parking.find(parking => parking.id === id)
+    parking.name = req.body.name,
+    parking.city = req.body.city,
+    parking.type = req.body.type,
+    res.status(200).json(parking)
+})
 
 app.listen(8080, () => {
     console.log('Serveur à l\'écoute');
