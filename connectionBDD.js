2d8b28b584603474edf92175cab0ad718b0f4966
Mickael Brunet <mickael.brunet64@gmail.com>
2021-07-23 13:35:37 +0200

    Importation des fichiers csv dans deux tables mysql créée au préalable : parking, reservation Importation des données du csv avec node et le fichier import_csv_to_database.js

--- connectionBDD.js
+++ connectionBDD.js
@@ -6,11 +6,21 @@ const db = mysql.createConnection({
  
     user: "root",
  
-    password: ""
+    password: "",
+
+    database: "api_rest"
  
 });
 
 db.connect(function(err) {
     if (err) throw err;
     console.log("Connecté à la base de données MySQL!"); //OK
+    //db.query("CREATE DATABASE api_rest", function (err, result) {
+    //    if (err) throw err;
+    //    console.log("Base de données créée !"); // api_rest créé
+    //  });
 });
+
+db.end();
+
+
