8410127727cd16c8c3a7aa023f285825f0b0bf82
Mickael Brunet <mickael.brunet64@gmail.com>
2021-07-25 10:51:27 +0200

    Vérification OK entre la communication entre reservation.js et connectionBDD.js

--- model/reservation.js
+++ model/reservation.js
@@ -1,6 +1,25 @@
-const connection = require('../repository/connectionBDD')
-const reservation = {}
+const config = require('../repository/connectionBDD')
+const mysql = require('mysql')
+const parking = {} // Un objet vide qui va contenir les méthodes
+// a exporté comme module
 
-reservation.list = async function () {
-    return await connection.db.collection('parkings').find({}).toArray()
-}
+let connection = mysql.createConnection(config)
+
+let sql = 'SELECT * FROM reservation';
+connection.query(sql, (error, results) => {
+  if (error) {
+    return console.error(error.message);
+  }
+  console.log(results);
+});
+
+/*
+mysql.query("SELECT * FROM parking", function(err, lignes) {
+    if (err) throw err
+    console.log(lignes)
+    //parking.enr = result;
+})
+
+//module.exports = parking
+
+*/
