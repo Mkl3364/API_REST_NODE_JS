da6ddf80d022ac875739d4e17d7dd415073b82f0
Mickael Brunet <mickael.brunet64@gmail.com>
2021-07-25 10:46:58 +0200

    connection OK entre le repository et le model

--- repository/connectionBDD.js
+++ repository/connectionBDD.js
@@ -16,6 +16,8 @@ db.connect(function(err) {
     if (err) throw err;
     console.log("Connecté à la base de données MySQL!"); //OK
     database = db;
+    database.user = db.user;
+
     });
 
 module.exports = database
